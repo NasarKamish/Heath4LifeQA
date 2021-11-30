79e5f65781461c85d1810330c831c4e494971e2c
Nasar <NasarKamish@gmail.com>
2021-11-30 09:41:51 +0200

    UPdated file

--- api/knexfile.js
+++ api/knexfile.js
@@ -1,34 +1,34 @@
 // Update with your config settings.
-const creds = require('./config/db');
+const creds = require("./config/db");
 
 module.exports = {
   development: {
-    client: 'pg',
+    client: "pg",
     connection: {
       database: process.env.DATABASE_NAME || creds.DATA,
       user: process.env.POSTGRES_USER || creds.USER,
       password: process.env.POSTGRES_USER_PW || creds.PASS,
       port: process.env.POSTGRES_PORT || creds.PORT,
-      host: process.env.POSTGRES_HOST || creds.HOST
+      host: process.env.POSTGRES_HOST || creds.HOST,
     },
     migrations: {
-      directory: './db/migrations'
+      directory: "./db/migrations",
     },
     seeds: {
-      directory: './db/seeds'
+      directory: "./db/seeds",
     },
   },
   production: {
-    client: 'pg',
+    client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
-      directory: './db/migrations'
+      directory: "./db/migrations",
     },
     seeds: {
-      directory: './db/seeds'
+      directory: "./db/seeds",
     },
     ssl: {
-      rejectUnauthorized: false
-    }
-  }
+      rejectUnauthorized: false,
+    },
+  },
 };
--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
+const express = require("express");
+const cookieParser = require("cookie-parser");
 const port = process.env.PORT || 3000;
-const cors = require('cors');
+const cors = require("cors");
 
 const app = express();
 
@@ -11,19 +11,19 @@ app.use(cors()); // corsConfig
 
 app.use(cookieParser());
 
-app.use(express.json({ limit: '5MB', extended: true}));
+app.use(express.json({ limit: "5MB", extended: true }));
 
-app.get('/', (req, res) => {
-    res.send('Health 4 Life API');
+app.get("/", (req, res) => {
+  res.send("Health 4 Life API");
 });
 
 // routes
-app.use('/api/user',require('./routes/user'));
-app.use('/api/admin', require('./routes/admin'));
-app.use('/api/areas', require('./routes/area'));
-app.use('/api/sites', require('./routes/sites'));
-app.use('/api/surveys', require('./routes/surveys'));
+app.use("/api/user", require("./routes/user"));
+app.use("/api/admin", require("./routes/admin"));
+app.use("/api/areas", require("./routes/area"));
+app.use("/api/sites", require("./routes/sites"));
+app.use("/api/surveys", require("./routes/surveys"));
 
-app.listen(port, function() {
-	console.log('Listening on port: ', port);
+app.listen(port, function () {
+  console.log("Listening on port: ", port);
 });
